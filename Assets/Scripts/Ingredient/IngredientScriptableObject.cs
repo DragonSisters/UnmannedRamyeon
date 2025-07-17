d43e5413d02a4cfb1852d6ed5f94fca400247930
charotiti9 <charotiti9@gmail.com>
2025-07-17 18:56:39 +0900

    재료 스크립터블 오브젝트 추가

---
+++ Assets/Scripts/Ingredient/IngredientScriptableObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ingredient", menuName = "Scriptable Objects/Ingredient")]
+public class IngredientScriptableObject : ScriptableObject
+{
+    [SerializeField] private string Name;
+    [SerializeField] private Sprite Icon;
+}
