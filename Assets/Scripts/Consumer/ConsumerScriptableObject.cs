eb06d988b1fdb49426442db90bdab1b6ae183c72
charotiti9 <charotiti9@gmail.com>
2025-07-17 17:49:05 +0900

    손님 이슈타임과 이슈타임까지의 대기시간 추가

--- Assets/Scripts/Consumer/ConsumerScriptableObject.cs
+++ Assets/Scripts/Consumer/ConsumerScriptableObject.cs
@@ -5,9 +5,10 @@ public enum ConsumerState
 {
     Invalid = -1, // 설정이 되지 않은, 정상이 아닌 상태입니다. 예외처리해야합니다.
     
-    Usual, // 아무것도 아닌 상태입니다.
     Enter, // 등장
     Exit, // 퇴장
+    Usual, // 아무것도 아닌 상태입니다.
+    Issue, // 이슈 상태입니다.
     Upset, // 화남
     Smile, // 좋음
 }
@@ -18,7 +19,7 @@ public class ConsumerScriptableObject : ScriptableObject
     /// <summary>
     /// 외형
     /// </summary>
-    public Sprite appearance;
+    [SerializeField] private Sprite appearance;
 
     [System.Serializable]
     public struct Dialogue
@@ -32,12 +33,47 @@ public class ConsumerScriptableObject : ScriptableObject
     public List<Dialogue> dialogues = new();
 
     /// <summary>
-    /// 체류 시간
-    /// </summary>
-    public float lifeTime;
-
-    /// <summary>
     /// 찾아오는 최소 날짜(단계)
     /// </summary>
-    public int appearDate;
+    [SerializeField] private int appearDate;
+
+    /// <summary>
+    /// 체류 시간
+    /// </summary>
+    [SerializeField] private float lifeTime;
+
+    /// <summary>
+    /// 최소/최대 일반상태 유지 시간
+    /// </summary>
+    [SerializeField] private float minUsualTime = 2f, maxUsualTime = 4f;
+    private float usualTime = -1f;
+
+    public Sprite Appearance => appearance;
+    public string GetDialogueFromState(ConsumerState state)
+    {
+        foreach (var dialogue in dialogues)
+        {
+            if (dialogue.state == state)
+            {
+                return dialogue.line;
+            }
+        }
+
+        throw new System.Exception($"손님의 대사 설정 중 {state} 상태로 설정된 대사가 없습니다.");
+    }
+    public int AppearDate => appearDate;
+    public float LifeTime => lifeTime;
+    public float UsualTime 
+    {
+        get 
+        {
+            // 아직 설정되지 않았을 때만 설정합니다.
+            if (usualTime <= 0)
+            {
+                usualTime = Random.Range(minUsualTime, maxUsualTime);
+            }
+
+            return usualTime;
+        } 
+    }
 }
