8e8247579d09a4fde6acc109e2a6cfe0382d0b0c
anditsoon <83330939+anditsoon@users.noreply.github.com>
2025-07-21 15:30:31 +0900

    가장 처음에 고른 재료 말풍선으로 표시

---
+++ Assets/Scripts/Consumer/ConsumerUI.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ConsumerUI : MonoBehaviour
+{
+    [SerializeField] private Image[] ingredientImages;
+    [SerializeField] private Image[] CorrectOrWrongImages;
+    [SerializeField] private Sprite[] CorrectOrWrongSprites;
+
+    public void UpdateIngredientImages(List<IngredientScriptableObject> ingredients)
+    {
+        for(int ingredientNum = 0; ingredientNum < ingredients.Count; ingredientNum++)
+        {
+            ingredientImages[ingredientNum].sprite = ingredients[ingredientNum].IconSprite;
+        }
+    }
+}
