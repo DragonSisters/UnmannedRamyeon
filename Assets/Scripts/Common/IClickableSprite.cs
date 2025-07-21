592b7fc922b60794413281ff07141b24520f6987
charotiti9 <charotiti9@gmail.com>
2025-07-21 14:48:16 +0900

    주석 추가

--- Assets/Scripts/Common/IClickableSprite.cs
+++ Assets/Scripts/Common/IClickableSprite.cs
@@ -1,5 +1,11 @@
 ﻿public interface IClickableSprite
 {
+    /// <summary>
+    /// 클릭이 가능한 타이밍에만 True
+    /// </summary>
     public bool IsClickable { get; }
+    /// <summary>
+    /// 클릭되면 실행할 로직을 이 안에 넣습니다
+    /// </summary>
     public void OnSpriteClicked();
 }
