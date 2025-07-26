51ac8943c36618e397229554f03bdccb20b96c63
charotiti9 <charotiti9@gmail.com>
2025-07-26 10:33:06 +0900

    이동매니저 추가 및 줄세우기 매니저 삭제 (이동매니저로 통일)

--- Assets/Scripts/Consumer/MoveManager.cs
+++ Assets/Scripts/Consumer/MoveManager.cs
@@ -2,22 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineUpManager : Singleton<LineUpManager>
+public class MoveManager : Singleton<MoveManager>
 {
+    [Header("입퇴장")]
+    [SerializeField] private Transform enterPoint;
+    [SerializeField] private Transform exitPoint;
+    [Header("줄서기")]
     [SerializeField] private int lineCount = 4;
     [SerializeField] private float lineSpacingFactor = 1f;
     [SerializeField] private Transform[] lineStartingPoint;
-    List<(Queue line, Vector2 startingPoint)> lineList = new();
+    
+    private List<(Queue line, Vector2 startingPoint)> lineList = new();
+    private List<IngredientScriptableObject> ingredientScriptableObjects = new();
 
-    // @charotiti9 TODO: 이동기능이 구현되면 GameManager에서 호출하도록 합시다
-    public void OnGameStart()
+    public void OnGameEnter()
     {
+        ingredientScriptableObjects = IngredientManager.Instance.IngredientScriptableObject;
         for (int i = 0; i < lineCount; i++)
         {
             lineList.Add((new Queue(), lineStartingPoint[i].position));
         }
     }
 
+    public Vector2 GetIngredientPoint(IngredientScriptableObject targetIngredient)
+    {
+        var point = Vector2.negativeInfinity;
+        foreach (var ingredient in ingredientScriptableObjects)
+        {
+            if(ingredient.name == targetIngredient.name)
+            {
+                break;
+            }
+        }
+
+        if(point == Vector2.negativeInfinity)
+        {
+            throw new System.Exception($"재료 {targetIngredient.name}를 찾을 수 없습니다. scriptableObject를 확인해주세요.");
+        }
+
+        return point;
+    }
 
     /// <summary>
     /// 손님이 처음에 줄을 고를 때, 어디 서있어야 하는지 계산하여 반환해줍니다.
@@ -57,7 +81,7 @@ public class LineUpManager : Singleton<LineUpManager>
         for (int i = 0; i < lineList.Count; i++)
         {
             var consumerCount = lineList[i].line.Count;
-            if(consumerCount < minConsumerCount)
+            if (consumerCount < minConsumerCount)
             {
                 minLineIndex = i;
                 minConsumerCount = consumerCount;
--- Assets/Scripts/GameSystem/GameManager.cs
+++ Assets/Scripts/GameSystem/GameManager.cs
@@ -52,6 +52,7 @@ public class GameManager : Singleton<GameManager>
         StartCoroutine(UpdateGame());
 
         ConsumerManager.Instance.StartSpawn();
+        MoveManager.Instance.OnGameEnter();
     }
 
     private IEnumerator UpdateGame()
