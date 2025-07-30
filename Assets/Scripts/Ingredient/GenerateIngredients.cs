aa6e5334fe8da193caf08562040393b128ee18a1
anditsoon <anditsoon@gmail.com>
2025-07-30 17:59:53 +0900

    SpriteRender 로 생성하는 방식으로 바꿈

---
+++ Assets/Scripts/Ingredient/GenerateIngredients.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+public class GenerateIngredients : MonoBehaviour
+{
+    void Start()
+    {
+        CreateIngredientImageOnPosition();
+    }
+
+    private void CreateIngredientImageOnPosition()
+    {
+        List<IngredientScriptableObject> ingredientList = IngredientManager.Instance.IngredientScriptableObject;
+        foreach (IngredientScriptableObject ingredient in ingredientList)
+        {
+            GameObject ingredientGameObj = new GameObject(ingredient.name);
+            ingredientGameObj.transform.SetParent(gameObject.transform, false); // 생성할 때는 로컬 좌표 유지
+            ingredientGameObj.transform.position = ingredient.Point; // 각 재료별 좌표로 옮김
+
+            // SpriteRenderer 생성
+            SpriteRenderer spriteRenderer = ingredientGameObj.GetComponent<SpriteRenderer>();
+            if (spriteRenderer == null)
+            {
+                spriteRenderer = ingredientGameObj.AddComponent<SpriteRenderer>();
+            }
+            spriteRenderer.sprite = ingredient.Icon;
+
+
+            ingredientGameObj.GetOrAddComponent<IngredientClick>();
+        }
+    }
+}
---
+++ Assets/Scripts/Ingredient/IngredientClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class IngredientClick : MonoBehaviour, IClickableSprite
+{
+    public bool IsClickable => isClickable;
+    private bool isClickable = true;
+
+    public void OnSpriteClicked()
+    {
+        Debug.LogError($"재료 아이템 {gameObject.name}이 클릭되었습니다.");
+    }
+
+    public void OnSpriteDeselected()
+    {
+        throw new System.NotImplementedException();
+    }
+}
