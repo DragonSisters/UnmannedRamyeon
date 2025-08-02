32ebacb35dce312eb22cae5c8840cd8036f27c60
anditsoon <anditsoon@gmail.com>
2025-08-03 00:08:24 +0900

    중복재료 선택 불가 처리 및 대사 추가

--- Assets/Scripts/Consumer/ThiefConsumer.cs
+++ Assets/Scripts/Consumer/ThiefConsumer.cs
@@ -18,4 +18,9 @@ public class ThiefConsumer : Consumer
     internal override void HandleChildUnclicked()
     {
     }
+
+    internal override IEnumerator HandleChildIssue()
+    {
+        yield break;
+    }
 }
