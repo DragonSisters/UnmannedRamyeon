a8a8460a195900b72f9aca037c253c9e5a27c79a
anditsoon <anditsoon@gmail.com>
2025-08-03 00:35:42 +0900

    사운드매니저 기본 틀

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    // AudioSource
+    public AudioSource BgmAudio;
+    public AudioSource PlayerAudio;
+    public AudioSource ConsumerAudio;
+
+    // audio clip 을 여러 개 담아 놓을 변수
+    public AudioClip[] BgmClips;
+    public AudioClip[] PlayerClips;
+    public AudioClip[] ConsumerClips;
+
+    private int startIndex = 0;
+
+    void Start()
+    {
+        PlayBgmSound(startIndex);
+    }
+
+    private void PlayBgmSound(int index)
+    {
+        BgmAudio.PlayOneShot(BgmClips[index]);
+    }
+}
