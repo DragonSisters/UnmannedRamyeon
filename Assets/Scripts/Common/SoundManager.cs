840037c6d3231d510f25a1f81a2c1bada67e1059
anditsoon <anditsoon@gmail.com>
2025-08-03 00:56:47 +0900

    사운드 클립들 넣었다

---
+++ Assets/Scripts/Common/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    // AudioSource
+    public AudioSource BgmAudio;
+    public AudioSource EffectAudio;
+
+    // audio clip 을 여러 개 담아 놓을 변수
+    public AudioClip[] BgmClips;
+    public AudioClip[] EffectClips;
+
+    private int startIndex = 0;
+
+    void Start()
+    {
+        PlayBgmSound(startIndex);
+    }
+
+    private void PlayBgmSound(int index)
+    {
+        BgmAudio.PlayOneShot(BgmClips[index]);
+    }
+}
