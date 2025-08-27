27cd74cb74b37392cbfcc051a254439772b23cbf
anditsoon <anditsoon@gmail.com>
2025-08-27 17:43:19 +0900

    냄비 날아오는/날아가는 애니메이션 추가

---
+++ Assets/Scripts/UI/PotUIController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotUIController : MonoBehaviour
+{
+    [SerializeField] private GameObject pot;
+    [SerializeField] private List<SpriteRenderer> ingredientsInPot;
+
+    private Vector2 pot_originalPos = new Vector2(0, 0);
+    [SerializeField] private Animation potAnim;
+    private float potAnimTime = 0.5f;
+
+    private Queue<IEnumerator> potQueue = new Queue<IEnumerator>();
+    private bool isPotQueueRunning = false;
+
+    public void EnqueuePotRoutine(IEnumerator routine)
+    {
+        potQueue.Enqueue(routine);
+        if (!isPotQueueRunning)
+            StartCoroutine(ProcessPotQueue());
+    }
+
+    private IEnumerator ProcessPotQueue()
+    {
+        isPotQueueRunning = true;
+        while (potQueue.Count > 0)
+        {
+            yield return StartCoroutine(potQueue.Dequeue());
+        }
+        isPotQueueRunning = false;
+    }
+
+    public IEnumerator BringPot()
+    {
+        pot.transform.position = pot_originalPos;
+        pot.SetActive(true);
+        potAnim.Play("pot_slideIn");
+
+        yield return new WaitForSeconds(potAnimTime);
+    }
+
+    public IEnumerator RemovePot()
+    {
+        potAnim.Play("pot_slideOut");
+        yield return new WaitForSeconds(potAnimTime);
+
+        foreach (SpriteRenderer spriteRenderer in ingredientsInPot)
+        {
+            spriteRenderer.gameObject.SetActive(false);
+        }
+
+        pot.SetActive(false);
+    }
+}
